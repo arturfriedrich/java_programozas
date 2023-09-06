5ab7c9ae22b097b10830360cc1a4604a0058832a
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-06 16:50:33 +0200

    hello csapat!

--- het_1/src/Teszt.java
+++ het_1/src/Teszt.java
@@ -1,7 +1,14 @@
 public class Teszt {
 
-    int keres() {
-        return 1;
+    public static void main(String[] args) {
+        /*
+            public - látható a program bármely részéből (public, protected, private, package)
+            static - osztályszintű változó, nem példányhoz kötött
+            void -   nincs visszatérési érték
+            main -   a program belépési pontja
+            String[] args - a program argumentumai
+        */
+        System.out.println("Hello csapat!");
     }
 
 }
