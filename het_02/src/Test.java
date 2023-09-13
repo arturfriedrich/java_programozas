3bf55f92c172954b4b2fd728d00eec097cfa3f4a
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-13 16:39:56 +0200

    setKor added

--- het_02/src/Test.java
+++ het_02/src/Test.java
@@ -4,6 +4,11 @@ public class Test {
 
         Kutya k1 = new Kutya("Buksi", 3, "vizsla");
 
+        // a setKor metódus csak akkor állítja be az értéket, ha az 1 és 15 között van
+        k1.setKor(4);
+        // a setKor metódus nem állítja be az értéket, mert -2 nem esik az 1 és 15 közé
+        k1.setKor(-2);
+
         System.out.println(k1.getNev());
         System.out.println(k1.getKor());
 
