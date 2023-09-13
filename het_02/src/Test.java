7b916efc24c3971cf932902dfa6ddbf334ecbee8
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-13 16:38:27 +0200

    made tester class

---
+++ het_02/src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        Kutya k1 = new Kutya("Buksi", 3, "vizsla");
+
+        System.out.println(k1.getNev());
+        System.out.println(k1.getKor());
+
+        System.out.println(k1.toString());
+
+    }
+
+}
