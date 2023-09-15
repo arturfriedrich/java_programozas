5397e17ac988b1818e4e65752c5f84369e699547
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-15 13:56:28 +0200

    ArviziKeresoKutya class constructed

--- het_02/src/ArviziKeresoKutya.java
+++ het_02/src/ArviziKeresoKutya.java
@@ -1,2 +1,18 @@
-public class ArviziKeresoKutya {
+public class ArviziKeresoKutya extends KeresoKutya {
+
+    private int merulesiMelyseg;
+
+    // Konstruktor
+    public ArviziKeresoKutya(String nev, int kor, String fajta, int keresesiHatekonysag, int merulesiMelyseg) {
+        super(nev, kor, fajta, keresesiHatekonysag);
+        this.merulesiMelyseg = merulesiMelyseg;
+    }
+
+    public int getMerulesiMelyseg() {
+        return merulesiMelyseg;
+    }
+
+    public String toString() {
+        return super.toString() + " Merülési mélység: " + merulesiMelyseg + "m.";
+    }
 }
