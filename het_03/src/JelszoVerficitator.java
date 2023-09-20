740711ca90ce1ba35505b454397994d334eab5a0
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-20 21:21:11 +0200

    passwd verificator finished

--- het_03/src/JelszoVerficitator.java
+++ het_03/src/JelszoVerficitator.java
@@ -2,7 +2,38 @@ public class JelszoVerficitator {
 
     public static void main(String[] args) {
 
-        // A power point-ban található a feladat, házi feladat megoldani
+        String password = "asb:cap3od";
+        boolean valid = false;
+
+        int length = password.length();
+        int letterCount = 0;
+        int numberCount = 0;
+        int specialCharacterCount = 0;
+
+        for (int i = 0; i < length; i++) {
+            char c = password.charAt(i);
+            if (Character.isLetter(c)) {
+                letterCount++;
+            } else if (Character.isDigit(c)) {
+                numberCount++;
+            } else {
+                specialCharacterCount++;
+            }
+        }
+
+        if (length >= 8 && letterCount >= 1 && numberCount >= 1 && specialCharacterCount >= 1) {
+            valid = true;
+        }
+
+        for (int i = 0; i < length - 1; i++) {
+            char c1 = password.charAt(i);
+            char c2 = password.charAt(i + 1);
+            if (c1 == c2) {
+                valid = false;
+            }
+        }
+
+        System.out.println("Your password is " + (valid ? "valid" : "invalid") + ".");
 
     }
 
