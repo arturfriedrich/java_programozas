a88328e7d6f531e2409ae9996c86cddd9e81e2e7
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-20 15:30:23 +0200

    added paros szamok amik hettel oszthatoak

---
+++ het_03/src/ParosSzamok.java
@@ -0,0 +1,13 @@
+public class ParosSzamok {
+
+    public static void main(String[] args) {
+
+        for (int i = -10; i <= 100; i++) {
+            if (i % 7 == 0 && i % 2 == 0) {
+                System.out.print(i + "\t");
+            }
+        }
+
+    }
+
+}
