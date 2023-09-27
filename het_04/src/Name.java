76edd3791752f607e419dc611e8f894aa8691479
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-09-27 14:59:29 +0200

    1. feladat

---
+++ het_04/src/Name.java
@@ -0,0 +1,13 @@
+public class Name {
+
+    public static void main(String[] args) {
+
+        String name = "Friedrich Artúr";
+        short age = 2001;
+        double PI = Math.PI;
+
+        System.out.println("A nevem: " + name + ", születési évem: " + age + ", PI: " + PI);
+
+    }
+
+}
