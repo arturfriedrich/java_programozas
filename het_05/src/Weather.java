605b51191806e3ed7fd4ad607f152f49093fa0a1
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-04 15:38:42 +0200

    String.format

---
+++ het_05/src/Weather.java
@@ -0,0 +1,9 @@
+public class Weather {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.format("%-24s%2d:%02d %10.2f°C \t" + "Is it raining: %s", "2023.10.04", 15, 35, 19.2, "No"));
+
+    }
+
+}
