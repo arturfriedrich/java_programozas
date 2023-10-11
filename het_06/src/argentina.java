e8cbf8f18b71e26aad82f207cc9add67ee0743f8
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-10-11 15:47:41 +0200

    argentina.java added

---
+++ het_06/src/argentina.java
@@ -0,0 +1,20 @@
+public class argentina {
+
+    public static void main(String[] args) {
+
+        StringBuffer csapat = new StringBuffer();
+        csapat.append("\t" + "\t" + "   •" + "\t" + "\t" + "  •" + "\t" + "\t" + "\n");
+        csapat.append("\t" + "\t" + "Alvarez" + "\t" + "\t" + "Messi" + "\t" + "\t" + "\n" + "\n");
+        csapat.append("\t" + "    •" + "\t" + "\t" + "\t" + "•" + "\t" + "\t" + "\t" + "•" + "\n");
+        csapat.append("\t" + "Mc Allister" + "\t" + "Fernandez" + "\t" + "De Paul" + "\t" + "\n" + "\n");
+        csapat.append("   •" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "•" + "\n");
+        csapat.append("Tagliafico" + "\t" + "\t" + "\t" + "\t" + "\t" + "\t" + "Molina" + "\n" + "\n");
+        csapat.append("\t" + "   •" + "\t" + "\t" + "\t" + "•" + "\t" + "\t" + "\t" + "•" + "\n");
+        csapat.append("\t" + "L Martinez" + "\t" + "Otamendi" + "\t" + "Romero" + "\t" + "\n" + "\n");
+        csapat.append("\t" + "\t" + "\t" + "\t" + "•" + "\t" + "\t" + "\t" + "\t" + "\n");
+        csapat.append("\t" + "\t" + "\t" + "E Martinez" + "\t" + "\t" + "\t" + "\n");
+
+        System.out.println(csapat.toString());
+    }
+
+}
